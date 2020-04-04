1c04507054cee497ff4bcca198edfd43aef0c989
ryosuke0206 <kyoho1988@gmail.com>
2020-04-04 14:42:42 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.12.1'
 set :application, 'freemarket_sample_67a'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:ryosuke0206/freemarket_sample_67a.git'
+set :repo_url,  'git@github.com:ChimaeraV8/freemarket_sample_67a.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
