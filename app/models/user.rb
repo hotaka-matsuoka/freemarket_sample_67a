788279f0df30ababd716e76236bee82d84dde222
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-04-16 00:15:10 +0900

    カスタムバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,50 @@ class User < ApplicationRecord
   VALID_NAME_REGEX = /\A[一-龥ぁ-ん]/
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
   validates :nickname,                           presence: true, length: { maximum:20 }
-  validates :email,                              presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  validates :family_name, :first_name,           presence: true, format: { with: VALID_NAME_REGEX }
-  validates :family_name_kana, :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+  validates :email,                              uniqueness: true
+  validate  :email_error
+  validate  :family_name_error
+  validate  :first_name_error
+  validate  :family_name_kana_error
+  validate  :first_name_kana_error
   validates :birth_day,                          presence: true
 
   has_one :address, dependent: :destroy
+
+  def email_error
+    if email.blank?
+        errors.add(:email, 'メールアドレスを入力してください')
+    elsif email.match(VALID_EMAIL_REGEX) == nil
+        errors.add(:email, 'のフォーマットが不適切です')
+    end
+  end
+  def family_name_error
+    if family_name.blank?
+      errors.add(:family_name, 'を入力してください')
+    elsif family_name.match(VALID_NAME_REGEX) == nil
+      errors.add(:family_name, 'を全角で入力してください')
+    end
+  end
+  def first_name_error
+    if first_name.blank?
+      errors.add(:first_name, 'を入力してください')
+    elsif first_name.match(VALID_NAME_REGEX) == nil
+      errors.add(:first_name, 'を全角で入力してください')
+    end
+  end
+  def family_name_kana_error
+    if family_name_kana.blank?
+      errors.add(:family_name_kana, 'を入力してください')
+    elsif family_name_kana.match(VALID_NAME_KANA_REGEX) == nil
+      errors.add(:family_name_kana, 'を全角で入力してください')
+    end
+  end
+  def first_name_kana_error
+    if first_name_kana.blank?
+      errors.add(:first_name_kana, 'を入力してください')
+    elsif first_name_kana.match(VALID_NAME_KANA_REGEX) == nil
+      errors.add(:first_name_kana, 'を全角で入力してください')
+    end
+  end
+
 end
