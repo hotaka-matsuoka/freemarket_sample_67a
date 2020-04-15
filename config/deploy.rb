c5074a02da9eacf5dfdf8c1e5bd22e98aa87b486
ChimaeraV8 <macshirious@icloud.com>
2020-04-16 07:42:52 +0900

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,8 @@ set :repo_url,  'git@github.com:ChimaeraV8/freemarket_sample_67a.git'
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
@@ -32,3 +34,23 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+
+desc 'upload master.key'#ここ注意
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/master.key', "#{shared_path}/config/master.key")#ここ注意
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
