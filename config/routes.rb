438c396d8ebc9d36f2ca951514c0bf356f14fab3
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-04-16 15:30:51 +0900

    会員登録ページのマークアップ

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
-  root 'top#index'
-  resources :top,  only:[:index]
   devise_scope :user do
+    get 'users', to: redirect("/users/sign_up")
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+  root 'top#index'
+  resources :top,  only:[:index]
 end
