e458b5f008786041e53389f68085c6017d5ca8f8
ChimaeraV8 <macshirious@icloud.com>
2020-04-16 20:42:55 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-desc 'upload master.key' do#ここ注意
+desc 'upload master.key' #ここ注意
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
