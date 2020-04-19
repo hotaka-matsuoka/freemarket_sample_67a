62e745a0dbdc0966c2dce97e39a8243a6ce7e1e6
ChimaeraV8 <macshirious@icloud.com>
2020-04-20 06:48:41 +0900

    Delete 20200412144700_add_index_to_exhibitions.rb

--- db/migrate/20200412144700_add_index_to_exhibitions.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexToExhibitions < ActiveRecord::Migration[5.2]
-  def change
-    add_column :exhibitions, :sales_status, :integer, default: 0
-    add_index  :exhibitions, :sales_status
-  end
-end
