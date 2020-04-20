d602405c1d4829b101de2129404187548934c6c9
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-04-20 16:11:20 +0900

    クレジットカード一覧の追加

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,7 +1,17 @@
 class CardController < ApplicationController
    require "payjp"
+   before_action :set_card
 
   def index
+    if @card.present?
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @card_info = customer.cards.retrieve(customer.default_card)
+      @card_brand = @card_info.brand
+      @exp_month = @card_info.exp_month.to_s
+      @exp_year = @card_info.exp_year.to_s.slice(2,3) 
+    end
+  
   end
 
   def new
@@ -46,4 +56,8 @@ class CardController < ApplicationController
       redirect_to action: "new"
   end
 
+  private
+  def set_card
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+  end
 end
