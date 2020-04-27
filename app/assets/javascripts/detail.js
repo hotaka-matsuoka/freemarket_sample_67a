ebc48470e58669015a9a01dbd2c2919ad541b6f7
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-04-27 17:22:54 +0900

    画像切替の実装

--- app/assets/javascripts/detail.js
+++ app/assets/javascripts/detail.js
@@ -1,12 +1,8 @@
 $(function() {
-  $(".preview, li").each(function(i) {
-    i = i+1;
-    $('.item' + i).hide();
+  $('.thumbnail li').click(function() {
+    var className = $(this).attr("class");
+    var num = className.slice(5);
+    $('.preview li').hide();
+    $('.item' + num).fadeIn();
   });
-    $('.thumbnail li').click(function() {
-      var className = $(this).attr("class");
-      var num = className.slice(5);
-      $('.preview li').hide();
-      $('.item' + num).fadeIn();
-    });
 });
