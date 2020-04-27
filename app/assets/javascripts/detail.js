95464206f87d16d4539d2dccab5a9f8620f7dc1d
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-04-27 22:43:14 +0900

    Update detail.js

--- app/assets/javascripts/detail.js
+++ app/assets/javascripts/detail.js
@@ -1,4 +1,4 @@
-$(function() {
+document.addEventListener("turbolinks:load",function () {
   $('.thumbnail li').click(function() {
     var className = $(this).attr("class");
     var num = className.slice(5);
