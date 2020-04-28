09b15854c5d4afdf76fe0e12d9dd1d35ef468aea
github5104 <gaaliker@gmail.com>
2020-04-28 23:23:08 +0900

    update carriewave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,16 +14,11 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'freemarketsample67a'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample67a'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chinaerav8-freemarket-sample-67a'
   else
     # 開発環境はlocalに保存
     config.storage :file
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  end  
-
-  config.fog_directory  = 'chinaerav8-freemarket-sample-67a'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chinaerav8-freemarket-sample-67a'
-
-
+  end 
 
 end
