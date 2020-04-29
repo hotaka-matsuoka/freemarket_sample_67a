7a3669312cef7a89113b791815ebfe7ca7004e2f
github5104 <gaaliker@gmail.com>
2020-04-30 00:15:21 +0900

    Update exhibition.rb

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -10,13 +10,13 @@ class Exhibition < ApplicationRecord
   has_many  :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   enum sales_status:    { on_sale: 0, sold_out: 1 }
-  validates :name,        presence: true, length: { maximum:40 }
-  validates :explanation, presence: true, length: { maximum:1000 }
-  validates :price,       numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 1000000 }
-  validates :price, :shipping_method_id, :shipping_date_id, :condition_id, :prefecture_id, :category_id, :user_id, presence: true
-  validates_associated :images
-  validates :images, presence: true
-  validate :images_size_validate
+  # validates :name,        presence: true, length: { maximum:40 }
+  # validates :explanation, presence: true, length: { maximum:1000 }
+  # validates :price,       numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 1000000 }
+  # validates :price, :shipping_method_id, :shipping_date_id, :condition_id, :prefecture_id, :category_id, :user_id, presence: true
+  # validates_associated :images
+  # validates :images, presence: true
+  # validate :images_size_validate
 
   IMAGE_MAX = 10
   def images_size_validate
