9011d0896f14065c5f68eacc6fa5afc817a6bcca
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-05-04 21:16:39 +0900

    画像編集追加

--- app/assets/javascripts/exhibitions.js
+++ app/assets/javascripts/exhibitions.js
@@ -17,83 +17,46 @@ document.addEventListener("turbolinks:load",function () {
     }
   });
 
-  
-  //DataTransferオブジェクトで、データを格納する箱を作る
-  var dataBox = new DataTransfer();
-  //querySelectorでfile_fieldを取得
-  var file_field = document.querySelector('input[type=file]')
-  //選択したfileのオブジェクトをpropで取得
-  var files = $('input[type="file"]').prop('files')[0];
-  //fileが選択された時に発火するイベント
-  $('#img-file').change(function(){
-    $.each(this.files, function(i, file){
-      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
-      var fileReader = new FileReader();
-      //DataTransferオブジェクトに対して、fileを追加
-      dataBox.items.add(file)
-      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に代入
-      file_field.files = dataBox.files
+  const buildFileField = (index)=> {
+    const html = `<div data-index="${index}" class="js-file_group">
+                    <input class="js-file" type="file"
+                    name="exhibition[images_attributes][${index}][image_url]"
+                    id="exhibition_images_attributes_${index}_image_url"><br>
+                    <div class="js-remove">削除</div>
+                  </div>`;
+    return html;
+  }
 
-      var num = $('.exhibition-image').length + 1 + i
-      fileReader.readAsDataURL(file);
-    //画像が10枚になったら超えたらドロップボックスを削除する
-      if (num == 10){
-        $('#image-box__container').css('display', 'none')   
-      }
-    //読み込みが完了すると、srcにfileのURLを格納
-    fileReader.onloadend = function() {
-      var src = fileReader.result 
-      var html= `<div class='exhibition-image' data-index="1" data-image="${file.name}">
-      <div class=' exhibition-image__content'>
-        <div class='exhibition-image__content--icon'>
-          <img src=${src} width="124" height="120" >
-        </div>
-      </div>
-      <div class='exhibition-image__operetion'>
-        <div class='exhibition-image__operetion--delete'>削除</div>
-      </div>
-    </div>`
-      //image_box__container要素の前にhtmlを差し込む
-      $('#image-box__container').before(html);
-    };
-      //image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変える。
-      $('#image-box__container').attr('class', `exhibition-num-${num}`)
-      if (num > 4) {
-        $('.exhibition-container').removeClass('img');
-      }
-    });
+  $('.hidden-destroy').hide();
+  const buildImg = (index, url)=> {
+    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+    return html;
+  }
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+
+  $('#image-box').on('change', '.js-file', function(e) {
+    const targetIndex = $(this).parent().data('index');
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+
+    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      img.setAttribute('src', blobUrl);
+    } else {  // 新規画像追加の処理
+      $('#previews').append(buildImg(targetIndex, blobUrl));
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    }
   });
-  //削除ボタンをクリックすると発火するイベント
-  $(document).on("click", '.exhibition-image__operetion--delete', function(){
-    //削除を押されたプレビュー要素を取得
-    var target_image = $(this).parent().parent()
-    //削除を押されたプレビューimageのfile名を取得
-    var target_name = $(target_image).data('image')
-    //プレビューがひとつだけの場合、file_fieldをクリア
-    if(file_field.files.length==1){
-      //inputタグに入ったファイルを削除
-      $('input[type=file]').val(null)
-      dataBox.clearData();
-      console.log(dataBox)
-    }else{
-      //プレビューが複数の場合
-      $.each(file_field.files, function(i,input){
-        //削除を押された要素と一致した時、index番号に基づいてdataBoxに格納された要素を削除する
-        if(input.name==target_name){
-          dataBox.items.remove(i) 
-        }
-      })
-      //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に再度代入
-      file_field.files = dataBox.files
-    }
-    //プレビューを削除
-    target_image.remove()
-    //image-box__containerクラスをもつdivタグのクラスを削除のたびに変更
-    var num = $('.exhibition-image').length
-    $('#image-box__container').show()
-    $('#image-box__container').attr('class', `exhibition-num-${num}`)
-    if (num < 5) {
-      $('#1').addClass('img');
-    }
-  })
+
+  $('#image-box').on('click', '.js-remove', function() {
+    const targetIndex = $(this).parent().data('index')
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
+    $(this).parent().remove();
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+    $(`img[data-index="${targetIndex}"]`).remove();
+  });
 });
