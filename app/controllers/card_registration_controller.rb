23a088cb91b30f9276e9a5226e6c63ce5984a06c
ChimaeraV8 <macshirious@icloud.com>
2020-05-07 22:14:01 +0900

    card-registration

--- app/controllers/card_registration_controller.rb
+++ app/controllers/card_registration_controller.rb
@@ -1,4 +1,35 @@
 class CardRegistrationController < ApplicationController
+  
+  require "payjp"
+  before_action :set_card
+
   def index
+  end
+
+  def new
+  end
+
+  def create
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(card: params['payjp-token'])
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      if @card.save
+        redirect_to root_path
+      else
+        redirect_to action: "new"
+      end
+    end
+  end
+
+  private
+  def set_card
+    if user_signed_in?
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      @card = Card.find_by(user_id: current_user.id) if Card.where(user_id: current_user.id).present?
+    else
+      redirect_to new_user_registration_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   root 'top#index'
 
   get 'mypage', to: 'mypage#index'
-  get 'card-registration',to: 'card_registration#index'
   get 'logout', to: 'logout#index'
+  get 'card-registration', to: 'card_registration#index'
+  get 'card-registration/new', to: 'card_registration#new'
+  post 'card-registration', to: 'card_registration#create'
 
   resources :top,        only:[:index]
   resources :exhibitions do
@@ -23,8 +25,6 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
     resources :users, only: [:index]
   end
-
-
 
   resources :card, except: :edit
 end
