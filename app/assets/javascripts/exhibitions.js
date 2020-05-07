43266b465adc4687e5d1f03e7b50873dd33e69c7
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-05-07 14:11:01 +0900

    Update exhibitions.js

--- app/assets/javascripts/exhibitions.js
+++ app/assets/javascripts/exhibitions.js
@@ -39,9 +39,9 @@ document.addEventListener("turbolinks:load",function () {
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
   lastIndex = $('.js-file_group:last').data('index');
   fileIndex.splice(0, lastIndex);
- 
-  $('#image-box').on('change', function() {
-    
+  sizingLabel();
+
+  function sizingLabel() {
     var i = $('.js-file_group').length
     if (i == 1 || i == 6) {
       $('.label-content').css('width', '100%');
@@ -56,28 +56,8 @@ document.addEventListener("turbolinks:load",function () {
     };
     id = $('.js-file_group:last').data('index');
     $('.label-box').attr({id: `label-box--${id}`,for: `exhibition_images_attributes_${id}_image_url`});
-  });
-
-  $('#image-box').on('click', '.js-remove', function() {
-    var i = $('.js-file_group').length
-    i -= 1
-    console.log(i);
-    if (i == 1 || i == 6) {
-      $('.label-content').css('width', '100%');
-    } else if (i == 2 || i == 7) {
-      $('.label-content').css('width', '80%');
-    } else if (i == 3 || i == 8) {
-      $('.label-content').css('width', '60%');
-    } else if (i == 4 || i == 9) {
-      $('.label-content').css('width', '40%');
-    } else if (i == 5 || i == 10) {
-      $('.label-content').css('width', '20%');
-    };
-  });
-
-
-
-
+  };
+ 
   $('#image-box').on('change', '.js-file', function(e) {
     var targetIndex = $(this).parent().data('index');
     var file = e.target.files[0];
@@ -90,22 +70,19 @@ document.addEventListener("turbolinks:load",function () {
       $('#image-box').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-    }
+    };
+    sizingLabel();
   });
 
   $('#image-box').on('click', '.js-remove', function() {
     var targetIndex = $(this).parent().parent().data('index');
     var hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
     var targetFile = $(`#exhibition_images_attributes_${targetIndex}_image_url`);
-   
     if (hiddenCheck) hiddenCheck.prop('checked', true);
     $(this).parent().parent().remove();
     $(targetFile).parent().remove();
-  
-    id = $('.js-file_group:last').data('index');
-    $('.label-box').attr({id: `label-box--${id}`,for: `exhibition_images_attributes_${id}_image_url`});
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
     $(`img[data-index="${targetIndex}"]`).remove();
+    sizingLabel();
   });
-
 });
