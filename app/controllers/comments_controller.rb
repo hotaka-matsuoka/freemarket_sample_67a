21d6ba9c0cd3804bd1e425091079dfd57fcb2916
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-05-19 13:15:55 +0900

    コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: @exhibition.user.id)
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 
   def set_exhibition
