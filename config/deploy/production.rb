b7397fb899fed473e174a9e5c7a70dc8839292df
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2020-11-20 18:35:38 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,4 +60,4 @@ set :unicorn_rack_env, "production"
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.95.184.18', user: 'ec2-user', roles: %w{app db web}
+server '18.178.64.229', user: 'ec2-user', roles: %w{app db web}
