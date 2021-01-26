6c5508a8ebe85d8f93ff5ec7ed47904971ba2b30
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2021-01-26 03:19:35 +0000

    デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:hotaka-matsuoka/freemarket_sample_67a.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/rails/freemarket_sample_67a"
+set :deploy_to, "/var/www/freemarket_sample_67a"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
