aea44c388659230f4e52b73f621c9519957aa1af
hotaka-matsuoka <matsuhota_mail108@icloud.com>
2021-01-26 12:04:52 +0900

    capstrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,55 +1,79 @@
-# config valid only for current version of Capistrano
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.13.0'
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.15.0"
 
-# Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_67a'
+set :application, "freemarket_sample_67a"
+set :repo_url, "git@github.com:hotaka-matsuoka/freemarket_sample_67a.git"
 
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:hotaka-matsuoka/freemarket_sample_67a.git'
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/var/www/rails/freemarket_sample_67a"
 
-set :linked_files, fetch(:linked_files, []).push("config/credentials.yml.enc")
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.6.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
-# どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/fsm.pem'] 
+# Default value for :pty is false
+# set :pty, true
 
-# プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+set :linked_files, fetch(:linked_files, []).push('config/settings.yml')
 
-# Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
 set :keep_releases, 5
+set :rbenv_ruby, '2.6.3'
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
+set :log_level, :debug
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
 namespace :deploy do
+  desc 'Restart application'
   task :restart do
     invoke 'unicorn:restart'
   end
 
-desc 'upload master.key' #ここ注意
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
+  desc 'Create database'
+  task :db_create do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:create'
+        end
       end
-      upload!('config/master.key', "#{shared_path}/config/credentials.yml.enc")#ここ注意
     end
   end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
-end
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+  desc 'Run seed'
+  task :seed do
+    on roles(:app) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+    end
+  end
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,11 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "54.168.237.82", user: "hotaka_matsuoka", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
 
 
 # role-based syntax
@@ -42,11 +40,7 @@ set :unicorn_rack_env, "production"
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, keys: '~/.ssh/freemarket_sample_rsa'
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
@@ -60,4 +54,3 @@ set :unicorn_rack_env, "production"
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.168.237.82', user: 'ec2-user', roles: %w{app db web}
