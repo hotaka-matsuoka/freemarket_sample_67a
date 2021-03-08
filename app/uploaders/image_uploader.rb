81685035ff75d81e1f929dfde3bd7fad5be5a004
HotakaMatsuoka <matsuhota_mail108@icloud.com>
2021-03-08 16:58:45 +0900

    Add: 画像のリサイズ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [120, 120]
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? # 開発環境の場合
